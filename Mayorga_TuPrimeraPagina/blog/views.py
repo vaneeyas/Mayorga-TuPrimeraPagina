30bec16a21468b19e93eabdf6f7daae06d4aa63f
Vanesa Yasmin Mayorga <vanesa.yasmin21@gmail.com>
2025-02-18 18:27:59 -0300

    Corrección en modelo de Cursos

--- Mayorga_TuPrimeraPagina/blog/views.py
+++ Mayorga_TuPrimeraPagina/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Curso
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,5 +7,5 @@ def index(request):
     return render(request, 'blog/index.html')
 
 def cursos_disponibles(request):
-    post_list = Post.objects.all()
-    return render(request, 'blog/cursos_disponibles.html', context={"posts": post_list})
+    miCurso = Curso.objects.all()
+    return render(request, 'blog/cursos_disponibles.html', context={"cursos": miCurso})
