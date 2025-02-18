0ca44244e7067c35f9db3bdd0b1cbca6d710d57f
Vanesa Yasmin Mayorga <vanesa.yasmin21@gmail.com>
2025-02-18 20:55:00 -0300

    Corrección de admin.py

--- Mayorga_TuPrimeraPagina/blog/admin.py
+++ Mayorga_TuPrimeraPagina/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Profesor, Curso, Estudiante
 
-# Register your models here.
+admin.site.register(Profesor)
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+
+
+from django.contrib import admin 
+from .models import Curso 
